3421862e4611b65e2ea5f6970d1c0c31b382bb3a
Jonathan Valencia <darkmanp2@yahoo.com>
2016-03-01 23:40:20 -0500

    fresh start with while loop and switch

--- Maze1.cpp
+++ Maze1.cpp
@@ -1,10 +1,9 @@
 #include "Maze1.h"
 #include <iostream>
 #include <cassert>
+#include <conio.h>
 
 using namespace std;
-
-void depthSearch(Position pos);
 
 Maze1::~Maze1()
 {
@@ -18,66 +17,32 @@ void Maze1::find_exit()
 // the stack path should be empty
 // This function does no input and no output
 {
-	
 	// FILL IN THE CODE FOR THIS METHOD
 	Position nbr, current;
 	direction d = DOWN;
 
-	M[start.row][start.col] = VISITED;
-	path.push(start);
+	current = start;
 
-	while (!validPosition(start.Neighbor(d))) {
-		d = next_direction(d);
-	}
-	current = start.Neighbor(d);
-
-	while (!path.empty()) {
-
-		if (current == exitpos) {
-			return;
-		}
-
+	while (!(current == exitpos)) {
 		switch (M[current.row][current.col]) {
-		
 		case OPEN:
-
-			//M[current.row][current.col] = VISITED;
-			for (; ;) {
-				if (M[current.row][current.col] == VISITED) {
-					break;
-				}
-				for (direction i = DOWN; i != NONE; i = next_direction(i)) {
-					if (validPosition(current.Neighbor(i)) &&  M[current.Neighbor(i).row][current.Neighbor(i).col] == OPEN) {
-						//M[current.row][current.col] = VISITED;
-						current = current.Neighbor(i);
-						M[current.row][current.col] = VISITED;
-						path.push(current);
-						
-					}
-				}
-			}
+			M[current.row][current.col] = VISITED;
+			path.push(current);
+			current = current.Neighbor(d);
 			break;
-
 		case VISITED:
-
-			path.pop();
-			current = path.top();
-
+			if (current == path.top()) {
+				path.pop();
+			}
+			d = next_direction(d);
+			current.Neighbor(d);
 			break;
-		
 		case WALL:
-
-			current = path.top();
-			
 			break;
 		}
-
-	}		
+	}
+	
 }
-
-
-
-
 
 
 
